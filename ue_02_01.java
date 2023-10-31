5df064d4e8c286ce68726c5edcad869ea49ab306
Matteo Meysel <s2310238052@fhooe.at>
2023-10-31 11:40:36 +0100

    ue_02_01 fertig gemacht

--- ue_02_01.java
+++ ue_02_01.java
@@ -1,21 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import lib.util.In;
 import lib.util.Out;
 
 public class ue_02_01 {
-// Eingabe:
-// eine positive, ganze Zahl
-// Problemstellung:
-// die Zahl soll in binärer Schriftweise (rückwärts) ausgegeben werden.
-// Ausgabe:
-// die Zahl in binärer Schriftweise (rückwärts).
+    // Eingabe:
+    // eine positive, ganze Zahl
+    // Problemstellung:
+    // die Zahl soll in binärer Schriftweise (rückwärts) ausgegeben werden.
+    // Ausgabe:
+    // die Zahl in binärer Schriftweise (rückwärts).
 
-// Binärifizierung:
-int dezI = In.readInt();
-int rmaininDezI;
-int binI[];
-//joe
-rmaininDezI = dezI / 2;
-binI[0] = dezI % 2;
+    // Binärifizierung:
+    public static void main(String args[]){
 
-Out.println("Please read backwards: " + binI);
+        int dezI = In.readInt();
+        List<String> binString = new ArrayList<String>();
+        
+        
+        if (dezI == 0){
+            binString.add("0");
+        }
+        while (dezI > 0){
+
+            if (dezI % 2 == 0){
+                binString.add("0");
+            }
+            else {
+                binString.add("1");
+            }
+
+            dezI = dezI / 2;
+
+        }
+
+        Out.println("Please read backwards: " + binString);
+        
+    }
+
 }
